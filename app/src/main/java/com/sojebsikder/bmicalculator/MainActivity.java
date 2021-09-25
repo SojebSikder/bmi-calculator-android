00d432b5d954c20051b79758ec60a8a7b6dc8939
Sojeb Sikder <sojebsikder@gmail.com>
2021-09-25 20:33:47 +0600

    main

---
+++ app/src/main/java/com/sojebsikder/bmicalculator/MainActivity.java
@@ -0,0 +1,60 @@
+package com.sojebsikder.bmicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnCalculate;
+    private Button btnClear;
+
+    private EditText editTextAge;
+    private EditText editTextHeight;
+    private EditText editTextWeight;
+
+    private RadioButton radioBtnMale;
+    private RadioButton radioBtnFemale;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCalculate = findViewById(R.id.btnCalculate);
+        btnClear = findViewById(R.id.btnClear);
+
+        editTextAge = findViewById(R.id.editTextAge);
+        editTextHeight = findViewById(R.id.editTextHeight);
+        editTextWeight = findViewById(R.id.editTextWeight);
+
+        radioBtnMale = findViewById(R.id.radioBtnMale);
+        radioBtnFemale = findViewById(R.id.radioBtnFemale);
+
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Double weight = Double.parseDouble(editTextWeight.getText().toString());
+                Double height = (Double.parseDouble(editTextHeight.getText().toString())) /100;
+
+                // Specify 2 decimal places
+                DecimalFormat df = new DecimalFormat("#.##");
+                df.setRoundingMode(RoundingMode.CEILING);
+                String result =  df.format(weight / (height * height));
+
+                Toast.makeText(getApplicationContext(),result.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
